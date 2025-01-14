9a59c14149b9033fa6a72741f91be47646282322
Obiead <pch37276@gmail.com>
2025-01-14 08:27:40 +0545

    Added getter methods to Student class

---
+++ Student1.java
@@ -0,0 +1,30 @@
+public class Student1 {
+    private String name;
+    private int age;
+
+    public Student1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void displayStudentDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
